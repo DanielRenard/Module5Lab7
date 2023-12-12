d47466588c62f42276331bca67804644c4ff0d74
DanielRenard <danieljuderenard@gmail.com>
2023-12-12 14:58:01 -0600

    swagger

--- lab7part2/controller/storeController.js
+++ lab7part2/controller/storeController.js
@@ -1,7 +1,7 @@
 const axios = require('axios')
 
-const getProducts = (req, res) => {
-  axios
+const getProducts = async (req, res) => {
+    await axios
     .get("https://fakestoreapi.com/products")
     .then((response) => {
       // console.log(response)
